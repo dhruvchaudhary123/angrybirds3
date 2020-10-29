73ea70ab4ab39b831e3c4997c0e5767a580b2f35
dhruvchaudhary123 <dhruvhcuahdary514@gmail.com>
2020-10-29 20:59:52 +0530

    chainiscreated

---
+++ chain.js
@@ -0,0 +1,37 @@
+class Chain
+{
+ constructor(BodyA,BodyB)
+ {
+   var option=
+   {
+    bodyA: BodyA,
+    bodyB: BodyB,
+    stiffness: 1,
+    length: 10
+
+   }
+
+   
+ this.chain = Constraint.create(option)
+  World.add(world,this.chain)
+  
+
+ }
+
+ display(){
+ var pointA= this.chain.bodyA.position
+ var pointB= this.chain.bodyB.position
+
+    strokeWeight(4);
+
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+   
+
+
+
+ }
+
+
+
+}
